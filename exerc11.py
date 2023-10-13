4d803dbab9ff32d128670989214590d5e7656d7b
stpurachina <143897006+stpurachina@users.noreply.github.com>
2023-10-13 10:01:45 -0300

    Create exerc11.py

---
+++ exerc11.py
@@ -0,0 +1,24 @@
+class Carro:
+
+    def __init__(self, quilometrosLitro):
+        self.quilometrosLitro = quilometrosLitro
+        self.qtdeCombustivel = 0
+
+    def adicionarGasolina(self, quantidade):
+        self.qtdeCombustivel += float(quantidade)
+
+    def andar(self, distancia):
+        gasto = distancia / self.quilometrosLitro
+        self.qtdeCombustivel -= gasto
+
+    def obterGasolina(self):
+        print(f'{self.qtdeCombustivel:.2f}')
+
+#Teste Classe
+meuFusca = Carro(15)
+# 15 quilometros por litro de combustivel.
+meuFusca.adicionarGasolina(20)
+# abastece com 20 litros de combustivel.
+meuFusca.andar(100)
+# anda 100 quilometros.
+meuFusca.obterGasolina()
